cb4425998b8373d2dc2ddd05f256760107354692
Divy027 <divyprajapati027@gmail.com>
2024-07-24 17:01:05 +0530

    addContract method

--- vrf_rng/scripts/test_contract.ts
+++ vrf_rng/scripts/test_contract.ts
@@ -1,10 +1,15 @@
 import { Provider, Wallet, WalletUnlocked, hashMessage } from "fuels";
 import { VrfLyncAbi__factory } from "../typegen";
 import { getRandomB256 } from "fuels";
-
+import {VrfImplAbi__factory} from "../contracts"
 const FUEL_RPC_URL = "https://testnet.fuel.network/v1/graphql";
+
 const CONTRACT_ID =
   "0x271fd38866c6aa3bafe5c9ae9306435c57250ab7608771adc45c64beec33abe8";
+
+const vrf_CONTRACT_ID = "0x749a7eefd3494f549a248cdcaaa174c1a19f0c1d7898fa7723b6b2f8ecc4828d"
+
+
 
   const generateSeed = () => {
     const seed = getRandomB256();
@@ -29,7 +34,11 @@ const CONTRACT_ID =
 
   const contract = VrfLyncAbi__factory.connect(CONTRACT_ID, wallet);
 
-  let get_fee = await contract.functions.rng_cost().get();
+  const Vrf = VrfImplAbi__factory.connect(vrf_CONTRACT_ID, wallet);
+
+  let get_fee = await contract.functions.rng_cost()
+  .addContracts([Vrf])
+  .get();
   console.log("rng cost fee : ", get_fee.value.toString());
 
   console.log("....... testing attempt 1 (1st seed) .......")
@@ -38,9 +47,12 @@ const CONTRACT_ID =
 
   console.log("seed1 generated: ",seed1.toString());
 
-  const txn_request = await contract.functions.request_random_number(seed1).callParams({
+  const txn_request = await contract.functions.request_random_number(seed1)
+  .callParams({
     forward: [get_fee.value.toString(), baseAssetId],
-  }).call();
+  })
+  .addContracts([Vrf])
+  .call();
   console.log("Transaction Id(request): ", txn_request.transactionId);
 
 
